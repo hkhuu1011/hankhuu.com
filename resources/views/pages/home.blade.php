bcd780abdd701e04d9f4bb3828bf7671e1616d9b
Han Khuu <h.khuu01@gmail.com>
2018-07-10 19:52:03 -0400

    modified PagesController

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,23 +1,8 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name')}}</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        Welcome        
+        Poops
 
-        <!-- CSS -->
-        <link rel="stylesheet" href="css/style.css" type="text/css">
-
-    </head>
-
-    <body>
-
-        @yield('content')
-        
-    </body>
-</html>
+@endsection
