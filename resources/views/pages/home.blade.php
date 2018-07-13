e6a43577b24cb8b2ce1c2dc04424a760d6d1ae11
Han Khuu <h.khuu01@gmail.com>
2018-07-12 20:21:20 -0400

    parallax layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,16 +2,57 @@
 
 @section('content')
 
-    {{-- Jumbotron --}}
-    <div class="jumbotron jumbotron-fluid jumbotron-bg-img">
-        <div class="container header">
-            <h1 class="header-text">thatsKhuu</h1>
-            <h5 class="sub-text">Front End Developer | Photographer</h5>
-        </div>
-    </div>
+	<div id="parallax">
 
-	{{-- About --}}
-	<div class="container section-about">
-		About
-	</div>
+		<section>
+			<div class="header-space">
+			</div>
+		</section>
+
+		<section>
+		    <div class="parallax-one">
+		      	<h1 class="header-text">thatsKhuu</h1>
+		        <h5 class="sub-text">Front End Developer | Photographer</h5>
+		    </div>
+		</section>
+
+<section>
+  <div class="block">
+    <p>About</p>
+    <p class="line-break margin-top-10"></p>
+    <p class="margin-top-10"></p>
+  </div>
+</section>
+
+<section>
+  <div class="parallax-two">
+    <h2>Projects</h2>
+  </div>
+</section>
+
+<section>
+  <div class="block">
+    <p>Projects</p>
+    <p class="line-break margin-top-10"></p>
+    <p class="margin-top-10"></p>
+  </div>
+</section>
+
+<section>
+  <div class="parallax-three">
+    <h2>Photos</h2>
+  </div>
+</section>
+
+<section>
+  <div class="block">
+    <p>Photos</p>
+    <p class="line-break margin-top-10"></p>
+    <p class="margin-top-10"></p>
+  </div>
+</section>
+  
+</div>
+
+
 @endsection
