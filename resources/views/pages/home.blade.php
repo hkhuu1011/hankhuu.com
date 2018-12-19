9b2bb0b90f8c16bb9e962a5a0fd92022f33f1623
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-12-19 11:13:42 -0500

    added container in sections

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -52,9 +52,11 @@
 
 		<section>
 			<div class="block">
-				<p>Projects</p>
-				<p class="line-break margin-top-10"></p>
-				<p class="margin-top-10"></p>
+				<div class="container">
+					<p>Projects</p>
+					<p class="line-break margin-top-10"></p>
+					<p class="margin-top-10"></p>
+				</div>
 			</div>
 		</section>
 
@@ -151,35 +153,37 @@
 
 		<section>
 			<div class="block">
-				<p>Contact</p>
-				<?php
-					// Supply a user id and an access token
-					$userid = "30758156";
-					$accessToken = "30758156.70679d8.954c4b9f252d4deb877bbb76621026a2";
+				<div class="container">
+					<p>Contact</p>
+					<?php
+						// Supply a user id and an access token
+						$userid = "30758156";
+						$accessToken = "30758156.70679d8.954c4b9f252d4deb877bbb76621026a2";
 
-					// Gets our data
-					function fetchData($url){
-					     $ch = curl_init();
-					     curl_setopt($ch, CURLOPT_URL, $url);
-					     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-					     curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-					     $result = curl_exec($ch);
-					     curl_close($ch);
-					     return $result;
-					}
+						// Gets our data
+						function fetchData($url){
+						     $ch = curl_init();
+						     curl_setopt($ch, CURLOPT_URL, $url);
+						     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+						     curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+						     $result = curl_exec($ch);
+						     curl_close($ch);
+						     return $result;
+						}
 
-					// Pulls and parses data.
-					$result = fetchData("https://api.instagram.com/v1/users/{$userid}/media/recent/?access_token={$accessToken}&count=5");
-					$result = json_decode($result);
-				?>
+						// Pulls and parses data.
+						$result = fetchData("https://api.instagram.com/v1/users/{$userid}/media/recent/?access_token={$accessToken}&count=5");
+						$result = json_decode($result);
+					?>
 
-				<?php foreach ($result->data as $post): ?>
-					<!-- Renders images. @Options (thumbnail,low_resoulution, high_resolution) -->
-					<a class="group" rel="group1" href="<?= $post->images->standard_resolution->url ?>"><img src="<?= $post->images->thumbnail->url ?>"></a>
-				<?php endforeach ?>
+					<?php foreach ($result->data as $post): ?>
+						<!-- Renders images. @Options (thumbnail,low_resoulution, high_resolution) -->
+						<a class="group" rel="group1" href="<?= $post->images->standard_resolution->url ?>"><img src="<?= $post->images->thumbnail->url ?>"></a>
+					<?php endforeach ?>
 
-				<!-- <p class="line-break margin-top-10"></p> -->
-			<p class="margin-top-10"></p>
+					<!-- <p class="line-break margin-top-10"></p> -->
+					<p class="margin-top-10"></p>
+				</div>
 			</div>
 		</section>
 </div>
