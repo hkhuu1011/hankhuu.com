85b63bc65044723d8d82c1e1422fd92e486b7170
Han Khuu <hankhuu@intuitionbrandmarketing.com>
2018-12-19 15:55:25 -0500

    added links to social icons

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -218,10 +218,10 @@
 				<div class="container">
 					<div class="content">
 						<ul class="list">
-			                <li><a href=""><img class="social-logos" src="/img/gmail.svg" /></a></li>
-			                <li><a href=""><img class="social-logos" src="/img/linkedin.svg" /></a></li>
-			                <li><a href=""><img class="social-logos" src="img/github.svg" /></a></li>
-			                <li><a href=""><img class="social-logos" src="/img/instagram.svg" /></a></li>
+			                <li><a href="mailto:h.khuu01@gmail.com?Subject=Hello%20Han!" target="_top"><img class="social-logos" src="/img/gmail.svg" /></a></li>
+			                <li><a href="https://linkedin.com/in/hankhuu" target="_blank"><img class="social-logos" src="/img/linkedin.svg" /></a></li>
+			                <li><a href="https://github.com/hkhuu1011" target="_blank"><img class="social-logos" src="img/github.svg" /></a></li>
+			                <li><a href="https://www.instagram.com/thatskhuu/?hl=en" target="_blank"><img class="social-logos" src="/img/instagram.svg" /></a></li>
 		            	</ul>
 					</div>
 				</div>
