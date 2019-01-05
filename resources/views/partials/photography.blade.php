7ed848c4d6a37c4d4d670f02e86c717be35358e3
Han Khuu <h.khuu01@gmail.com>
2019-01-05 18:56:20 -0500

    added photography section to partials

---
+++ resources/views/partials/photography.blade.php
@@ -0,0 +1,116 @@
+<section class="photography hover-underline">
+    <div class="block">
+      <div class="container">
+        <div class="section-title sliding-u-l-r">Photography</div>
+      </div>
+      <div class="section-underline"></div>
+      <!-- SVG wrapper -->
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="svg-wrapper">
+              <!-- <svg xmlns="http://www.w3.org/2000/svg"> -->
+                <!-- <rect class="outline"/> -->
+                  {{-- Adriana's Gallery --}}
+                  <div id="lightgallery">
+                    <a href="/img/photography/DSC_6107_FinalCopy.jpg">
+                      <img class="photo-preview" src="/img/photography/DSC_6107_FinalCopy.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_6054_FinalCopy.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_6054_FinalCopy.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_6097_FinalCopy.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_6097_FinalCopy.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_6048_FinalCopy.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_6048_FinalCopy.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_6119_FinalCopy.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_6119_FinalCopy.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_6149_FinalCopy.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_6149_FinalCopy.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_6165_FinalCopy.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_6165_FinalCopy.jpg" />
+                    </a>
+                  </div>
+              <!-- </svg> -->
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="svg-wrapper">
+              <!-- <svg xmlns="http://www.w3.org/2000/svg"> -->
+                <!-- <rect class="outline"/> -->
+                  {{-- New York Gallery --}}
+                  <div id="lightgallery2">
+                    <a href="/img/photography/NewYork.jpg">
+                      <img class="photo-preview" src="/img/photography/NewYork.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork2.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork2.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork3.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork3.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork4.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork4.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork5.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork5.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork6.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork6.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork7.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork7.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork8.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork8.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork9.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork9.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork10.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork10.jpg" />
+                    </a>
+                    <a href="/img/photography/NewYork11.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/NewYork11.jpg" />
+                    </a>
+                  </div>
+              <!-- </svg> -->
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="svg-wrapper">
+              <!-- <svg xmlns="http://www.w3.org/2000/svg"> -->
+                <!-- <rect class="outline"/> -->
+                  {{-- Sammatha's Gallery --}}
+                  <div id="lightgallery3">
+                    <a href="/img/photography/DSC_7535_Final.jpg">
+                      <img class="photo-preview" src="/img/photography/DSC_7535_Final.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_7526_Final.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_7526_Final.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_7544_Final.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_7544_Final.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_7606_Final.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_7606_Final.jpg" />
+                    </a>
+                    <a href="/img/photography/DSC_7608_Final.jpg">
+                      <img style="display: none;" class="photo-preview" src="/img/photography/DSC_7608_Final.jpg" />
+                    </a>
+                  </div>
+              <!-- </svg> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <!-- <div class="right"><a href="/photos/more">More photos</a></div> -->
+      <small class="right" style="color:#ccc">(More coming soon)</small>
+    </div>
+  </section>
