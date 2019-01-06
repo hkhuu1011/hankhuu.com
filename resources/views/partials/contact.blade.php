9706e48b5aab70f707df351f5ef54ec2ed17a4d5
Han Khuu <h.khuu01@gmail.com>
2019-01-05 23:03:37 -0500

    added images to projects

---
+++ resources/views/partials/contact.blade.php
@@ -0,0 +1,18 @@
+<section class="contact hover-underline">
+    <div class="block">
+      <div class="container">
+        <div class="section-title sliding-u-l-r">Contact</div>
+      </div>
+      <div class="section-underline"></div>
+      <div class="container">
+        <div class="content">
+          <ul class="list">
+            <li><a href="mailto:h.khuu01@gmail.com?Subject=Hello%20Han!" target="_top"><img class="social-logos" src="/img/gmail.svg" /></a></li>
+            <li><a href="https://linkedin.com/in/hankhuu" target="_blank"><img class="social-logos" src="/img/linkedin.svg" /></a></li>
+            <li><a href="https://github.com/hkhuu1011" target="_blank"><img class="social-logos" src="img/github.svg" /></a></li>
+            <li><a href="https://www.instagram.com/thatskhuu/?hl=en" target="_blank"><img class="social-logos" src="/img/instagram.svg" /></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
--- resources/views/partials/projects.blade.php
+++ resources/views/partials/projects.blade.php
@@ -5,9 +5,21 @@
       </div>
         <div class="section-underline"></div>
         <div class="container">
-          <p class="content">COMING SOON</p>
-          <!-- <p class="line-break margin-top-10"></p>
-          <p class="margin-top-10"></p> -->
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+              <a href="https://bandbasher.com" target="_blank">
+                <img class="photo-preview projects" src="/img/projects/bandbasher.jpg" />
+              </a>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+              <a href="https://github.com/hkhuu1011/Recipes" target="_blank">
+                <img class="photo-preview projects" src="/img/projects/recipes.jpg" />
+              </a>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+              <img class="photo-preview projects" src="/img/photos.jpg" />
+            </div>
+          </div>
 
           <a href="/Resume2019.docx" download="resume_han_khuu" class="btn btn-default btn-margin" role="button">Download Resume</a>
         </div>
